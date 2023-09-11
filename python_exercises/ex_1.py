326400ab7843c1604d01dbb7cc82d180cc78886f
Leonardohas <leonardohas83@gmail.com>
2023-09-11 13:12:37 -0300

    added exercice description

---
+++ python_exercises/ex_1.py
@@ -0,0 +1,8 @@
+'''
+Num frigorífico existem 10 bois. Cada boi traz preso em seu pescoço 
+uma etiqueta contendo seu número
+de identificação e seu peso. Fazer um algoritmo que escreva:
+o número e o peso do boi mais gordo, o número e o peso do boi mais magro 
+e o peso médio dos bois.
+
+'''
