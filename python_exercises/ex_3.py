e1a0b9d6a7d4a8fffb3145877fdfec6a5ae86486
Leonardohas <leonardohas83@gmail.com>
2023-09-13 13:33:25 -0300

    added activity description

---
+++ python_exercises/ex_3.py
@@ -0,0 +1,6 @@
+'''
+Fazer um algoritmo, utilizando o comando enquanto ...
+faça, que: Leia um conjunto de 5 fichas contendo cada uma,
+a idade de um indivíduo e calcule e escreva a idade média deste grupo de indivíduos.
+
+'''
