722b36055804ef0675950769b528ea980e9fc77f
Leonardohas <leonardohas83@gmail.com>
2023-09-14 19:13:34 -0300

    exercicie resolution

--- python_exercises/ex_4.py
+++ python_exercises/ex_4.py
@@ -10,3 +10,34 @@ I.	 qual é o maior e qual é o menor índice de acidentes de trânsito e a que 
 II.	 qual é a média de veículos nas cinco cidades juntas;
 
 '''
+
+smaller_city = ""
+bigger_city = ""
+city = 0
+n_cidade = 0
+number_vehicles = 0
+number_accidents = 0
+count = 0
+minor_index = 0
+higher_index = 0
+sum = 0
+avarege = 0
+city = int(input("digite um numero de cidades brasileiras: "))
+
+while (count < city):
+  n_cidade = (input("Digite o nome da cidade: "))
+  number_vehicles = int(input("Digite o numero de veiculos: "))
+  number_accidents = int(input("Digite o numero de acidentes de trânsito com vitimas: "))
+  sum = number_vehicles + sum
+  count = count + 1
+  if (number_accidents < minor_index or minor_index == 0):
+    minor_index = number_accidents
+    smaller_city = n_cidade
+  elif (number_accidents > higher_index):
+    indice_maior = number_accidents
+    bigger_city = n_cidade
+
+avarege = "{:.2f}".format(sum / city)
+print("O maior indidice de acidente é " + str (indice_maior) + " na cidade: " + str (bigger_city) )
+print("O menor indidice de acidente é " + str (minor_index) + " na cidade: " + str (smaller_city) )
+print("A media de veiculos nas " + str (city) + " cidades é: " + str (avarege) )
