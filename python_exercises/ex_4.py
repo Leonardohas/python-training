072fb014d7b699205e97f80ba0d0335b71f0ab58
Leonardohas <leonardohas83@gmail.com>
2023-09-14 19:02:51 -0300

    added activity description

---
+++ python_exercises/ex_4.py
@@ -0,0 +1,12 @@
+'''
+Foi feita uma estatística em n cidades brasileiras para coletar dados sobre
+acidentes de trânsito.
+Foram obtidos os seguintes dados:
+a.   nome da cidade;
+b.	 número de veículos de passeio;
+c.	número de acidentes de trânsito com vítimas.
+Deseja-se saber:
+I.	 qual é o maior e qual é o menor índice de acidentes de trânsito e a que cidades pertencem;
+II.	 qual é a média de veículos nas cinco cidades juntas;
+
+'''
