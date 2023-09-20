c4a2fdc7ddb93becf2f6c54bc9125f562a39895c
Leonardohas <leonardohas83@gmail.com>
2023-09-20 13:22:37 -0300

    added activity description

---
+++ python_exercises/ex_7.py
@@ -0,0 +1,15 @@
+'''
+Exiba a tabuada de um número (1 a 10) fornecido pelo usuário.
+Por exemplo se ele digitar o número 5, então será mostrado:
+5	x	1	=	5
+5	x	2	=	10
+5	x	3	=	15
+5	x	4	=	20
+5	x	5	=	25
+5	x	6	=	30
+5	x	7	=	35
+5	x	8	=	40
+5	x	9	=	45
+5 x 10 =	50
+
+'''
